e3abd608fc5f897585fbe333d99b0ccdb27ee871
vladyslavnUA <vladyslavn@yahoo.com>
2020-05-09 04:07:50 -0700

    updated core urls

--- codefuse/urls.py
+++ codefuse/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('accounts/', include('accounts.urls'))
+    # path('accounts/', include('django.contrib.auth.urls')),
+    # path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
