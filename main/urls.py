1d6ffc942b918840970b5218f38e040f42e42dfb
vladyslavnUA <vladyslavn@yahoo.com>
2020-05-08 17:33:19 -0700

    updated urls

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path, include
-
 from . import views
+from .views import (
+    home,
+    ProjectList,
+    ProjectDetail,
+    ProjectCreate,
+    ProjectUpdate,
+    ProjectDelete,
+)
+
+app_name = 'projects'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('projects/', ProjectList.as_view(), name='reference'),
+    path('projects/add-project/', ProjectCreate.as_view(), name='add_project'),
+    path('projects/<slug:slug>/edit/', ProjectUpdate.as_view(), name='edit_project'),
+    path('projects/<slug:slug>/delete/', ProjectDelete.as_view(),
+         name='remove_project'),
+    path('projects/<slug:slug>/', ProjectDetail.as_view(), name='details'),
 ]
